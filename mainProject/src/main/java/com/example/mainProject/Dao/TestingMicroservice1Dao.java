78c23d53f89fb82d7fcea246526f28780ff4c389
Kavithau <85679333+kavithau73@users.noreply.github.com>
2024-01-10 17:15:13 +0530

    Add files via upload

---
+++ mainProject/src/main/java/com/example/mainProject/Dao/TestingMicroservice1Dao.java
@@ -0,0 +1,15 @@
+package com.example.mainProject.Dao;
+
+
+/**
+ * @author kavitha
+ * Interface layer
+ */
+public interface TestingMicroservice1Dao {
+
+
+	String addTwoString(String value1, String value2);
+
+	String getTestResult();
+
+}
